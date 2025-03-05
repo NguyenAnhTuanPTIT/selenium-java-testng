2abb5732d24ff4d03fb8989d16db140585c5d8de
Tuan Anh Nguyen <nguyenanhtuanptit123@gmail.com>
2025-03-05 23:56:27 +0700

    Update next

--- selenium_source_Folder/javaSDeT/Topic_01_DataType.java
+++ selenium_source_Folder/javaSDeT/Topic_01_DataType.java
@@ -1,4 +1,15 @@
 package javaSDeT;
 
 public class Topic_01_DataType {
+    //2 nhóm kiểu dữ liệu
+    //Nhóm 1 - Kiểu dữ liệu nguyên thủy
+    //char: ký tự
+    //byte/ short / int/ long: số nguyên
+    //float/ double: số thực
+    //boolean: logic
+
+    //Nhóm 2 - Kiểu dữ lệu tham chiêú
+
+
+    //Access Modifier: phạm vi truy cập (private/public/protected/default)
 }
