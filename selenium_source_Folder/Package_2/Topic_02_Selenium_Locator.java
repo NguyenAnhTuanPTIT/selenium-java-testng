f045ba3f489171161ba9923e53f6cc27c3e8c8f2
Tuan Anh Nguyen <nguyenanhtuanptit123@gmail.com>
2025-03-19 00:16:34 +0700

    Update 00:16 18/03

--- selenium_source_Folder/Package_2/Topic_02_Selenium_Locator.java
+++ selenium_source_Folder/Package_2/Topic_02_Selenium_Locator.java
@@ -105,26 +105,78 @@ public class Topic_02_Selenium_Locator {
 
     @Test
     public void TC_05_Partial_Link_Text() {
+    // Chỉ làm việc với element là link
+    // Có thể lấy hết toàn bộ text hoặc 1 phần (hay dùng)
 
+        driver.findElement(By.partialLinkText("Digital")); //Linktext hoàn chỉnh là Digital downloads
 
+        driver.findElement(By.partialLinkText("downloads"));
     }
 
     @Test
     public void TC_06_Tagname() {
+        // Tên thẻ (HTML)
+        // Tìm tất cả element giống nhau (thẻ của component giống nhau)
+        // Thường dùng để tìm tất cả các textbox/ checkbox/ radio/ link/ button/...
+        driver.findElements(By.tagName("button"));
 
+        driver.findElements(By.tagName("input"));
+
+        driver.findElements(By.tagName("label"));
 
     }
 
     @Test
     public void TC_07_Css() {
+        // CSS có thể cover việc tìm element theo: ID/ Class/ Name/ LinkText/ Partial LinkText/ Tagname
+        // CSS cũng có thể cover Xpath, nhưng một số trường hợp sẽ không dùng được
+        driver.findElement(By.cssSelector("input#Company")); // Tìm theo id của element
+        driver.findElement(By.cssSelector("#Company")); // Có thể viết tắt không cần dùng tên thẻ
+        driver.findElement(By.cssSelector("input[id='Company']")); // Viết theo kiểu chuẩn
 
+        driver.findElement(By.cssSelector("button.register-next-step-button")); // Tìm theo class của element (không lấy hết giá trị của class)
+        driver.findElement(By.cssSelector("button[class='button-1 register-next-step-button']")); // Nếu viết theo kiểu chuẩn thì giá trị của class phải lấy hết
+
+        driver.findElement(By.cssSelector("input[name='Password']")); // Tìm element theo name thì phải viết kiểu chuẩn
+
+        // Tìm element theo LinkText, do CSS ko tìm theo text được nên sẽ dựa vào các attribute khác để tìm
+        // Dùng "*=" để tìm một phần giá trị
+        driver.findElement(By.cssSelector("a[href*='register']"));
+
+        // Tìm theo element theo partial linktext cũng dùng tương tự như trên
+
+        // Tìm element theo tag name thì truyền tên thẻ vào
+        driver.findElements(By.cssSelector("a")); // Tìm tất cả thẻ a
+        driver.findElements(By.cssSelector("input")); // Tìm tất cả thẻ input
+        driver.findElements(By.cssSelector("button")); // Tìm tất cả thẻ button
 
     }
 
     @Test
     public void TC_08_XPath() {
+        // Xpath có thể cover việc tìm element theo: ID/ Class/ Name/ LinkText/ Partial LinkText/ Tagname và thậm chí cả CSS
+        driver.findElement(By.xpath("//input[@id='small-searchterms']")); // Dạng chuẩn
+        driver.findElement(By.xpath("//input[@id='FirstName']"));
+        driver.findElement(By.xpath("//input[@id='LastName']"));
 
+        // Tìm element theo class
+        driver.findElement(By.xpath("//button[@class='button-1 register-next-step-button']"));
+        driver.findElement(By.xpath("//button[contains(@class, 'register-next-step-button')]")); // Nếu chỉ lấy một phần trong class
 
+        // Tìm element theo name
+        driver.findElement(By.xpath("//input[@name='FirstName']"));
+
+        // Tìm element theo tagname
+        driver.findElement(By.xpath("//a"));
+        driver.findElement(By.xpath("//button"));
+        driver.findElement(By.xpath("//input"));
+
+        // Tìm element theo linktext
+        driver.findElement(By.xpath("//a[text()='Register']"));
+        driver.findElement(By.xpath("//a[text()='Shipping & returns']"));
+
+        // Tìm element theo partial linktext
+        driver.findElement(By.xpath("//a[contains(text(),'returns')]"));
     }
 
     //3- Clean: Delete data test/account/close browser/..
