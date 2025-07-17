7fc5facd9d2910e8064304eef0df25f26dcf01d3
NguyenAnhTuan <nguyenanhtuanptit123@gmail.com>
2025-07-17 23:59:11 +0700

    Update 23:58 16/07

--- selenium_source_Folder/Package_2/Topic_27_Wait_Find_Element.java
+++ selenium_source_Folder/Package_2/Topic_27_Wait_Find_Element.java
@@ -1,8 +1,7 @@
 package Package_2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Topic_00_Template {
+public class Topic_27_Wait_Find_Element {
     //1- Setup: OS/Browser/Web/Page/ Data/Variable/Object/..
 
     WebDriver driver;
@@ -19,18 +18,30 @@ public class Topic_00_Template {
     public void initalBrowser(){
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // Set total time  = 13s
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(13));
 
     }
 
     //2- Action/Excute: Tương tác lên Element nào/nhập liệu/Verify/....
     @Test
-    public void TC_01_() {
-        driver.get("");
+    public void TC_01_FindElement() {
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+
+        // 1 - Nếu Tìm thấy duy nhất element
+        // Output: trả về đúng element đó
+        // Không cần chờ hết total time của implicit
+        driver.findElement(By.cssSelector("input#FirstName"));
+
+        // 2 - Nếu tìm thấy nhiều hơn 1 element
+        driver.findElement(By.cssSelector("input#FirstName"));
+
+        // 3 - Nếu không tìm thấy element nào
     }
 
     @Test
-    public void TC_02_() {
+    public void TC_02_FindElements() {
 
     }
 
